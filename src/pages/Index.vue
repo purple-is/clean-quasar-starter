5fca3db9bb8d5b7f005486527e5c763116b455c0
Josh Mellicker <josh@platformpurple.com>
2021-08-25 15:41:18 -0700

    add axios and uploader

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <h3>nothing here yet</h3>
+    <div class="q-pa-md">
+      <div class="q-gutter-sm row items-start">
+        <q-uploader
+          url="http://localhost:4444/upload"
+          style="max-width: 300px"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
